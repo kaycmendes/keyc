241c8a8a20baedb6af67cec183f260f4b137e858
kaycmendes <kaycmendes@gmail.com>
2025-02-27 05:06:00 +0000

    Add compressor interface for data compression

--- src/compressors/base.ts
+++ src/compressors/base.ts
@@ -1,4 +1,19 @@
+/**
+ * Interface for compression adapters in Keyc
+ * Enables different compression algorithms to be used with Keyc
+ */
 export interface Compressor {
+  /**
+   * Compress data before storage
+   * @param data The data to compress
+   * @returns Promise resolving to the compressed data
+   */
   compress(data: any): Promise<any> | any;
+  
+  /**
+   * Decompress data after retrieval
+   * @param data The compressed data to decompress
+   * @returns Promise resolving to the original data
+   */
   decompress(data: any): Promise<any> | any;
 } 
